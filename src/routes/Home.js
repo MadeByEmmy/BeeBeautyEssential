5df503ebf16ac3aa697480ddd38ec89ddc56ff61
I_am_Emmanuel <emmaenwereji@gmail.com>
2024-01-06 15:28:59 +0100

    add all files

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,6 +1,10 @@
 import Carousel from 'react-bootstrap/Carousel';
 import "../components/Home.css"
-import {ceoimage} from "../assets/ceo.jpg"
+import ceoimage from "../assets/7.jpg"
+import inspired from "../assets/8.jpg"
+import sourced from "../assets/9.jpg"
+import backed from "../assets/10.jpg"
+import { Link } from 'react-router-dom';
 
 
 var heroData = [
@@ -26,18 +30,22 @@ var heroData = [
   },
   {
     id: 6,
-    image: require('../assets/6.jpg'),
+    image: require('../assets/6.jpg')
   }
 ]
 
 function AppHero() {
   return (
+    <>
       <div className='homepage'>
         <div className='aboutus'>
             <div className='aboutus-desp'>
               <h1>About Us</h1>
               <p>BeeBeauty Essentials is a fully African beauty and health focused brand creating a range of essentials for your skin and hair care using locally sourced natural ingredients which are neatly formulated and handmade with love to give you the best treatment you deserve.
               <br /> <br /> Our mission is to give you the confidence to wear the skin you like.</p>
+              <Link to="/Beebeauties">
+                <button className='button'>BeeBeauties</button>
+              </Link>
             </div>
             <section id="home" className="hero-block">
               <Carousel>
@@ -57,10 +65,21 @@ function AppHero() {
               </Carousel>
             </section>
         </div>
-        <div>
+        <div className='ceoimage-desp'>
           <img src={ceoimage} />
         </div>
       </div>
+      <div className='inspirationalquotes'>
+          <div className='inspirdesp'>
+              <img src={inspired} />
+              <p>INSPIRED BY BEAUTY</p>
+              <img src={sourced} />
+              <p>SOURCED FROM NATURE</p>
+              <img src={backed} />
+              <p>BACKED BY SCIENCE</p>
+          </div>
+        </div>
+    </>
   );
 }
 
