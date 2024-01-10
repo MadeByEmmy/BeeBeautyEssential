62bccc4495f53299c2b7a2456a579e4b7a944e87
I_am_Emmanuel <emmaenwereji@gmail.com>
2024-01-10 16:38:50 +0100

    add all files

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React, {useRef, useState} from "react";
+//importing Email.js 
+import emailjs from '@emailjs/browser';
 import "../components/Home.css"
 import ceoimage from "../assets/7.jpg"
 import inspired from "../assets/8.jpg"
 import sourced from "../assets/9.jpg"
 import backed from "../assets/10.jpg"
-import bestseller from "../assets/1.jpg"
+import bestseller from "../assets/result.png"
 import { Link } from 'react-router-dom';
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
@@ -76,12 +78,29 @@ const data = [
 function AppHero() {
   //styling the stars
   const style = { color: "gold" }
+  //Subcription form
+  const form = useRef()
+
+  //sending Emailjs function
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_cc1h1ew', 'template_oucmmak', form.current, 'p8yx2bdhcQYvRykI7')
+      .then(() => {
+          // change button text
+
+      }, (error) => {
+          console.log(error.text);
+      });
+      e.target.reset()
+  };
+  
   return (
     <>
       <div className='homepage'>
         <div className='aboutus'>
             <div className='aboutus-desp'>
-              <h1>About Us</h1>
+              <h2>About Us</h2>
               <p>BeeBeauty Essentials is a fully African beauty and health focused brand creating a range of essentials for your skin and hair care using locally sourced natural ingredients which are neatly formulated and handmade with love to give you the best treatment you deserve.
               <br /> <br /> Our mission is to give you the confidence to wear the skin you like.</p>
               <Link to="/Beebeauties">
@@ -107,15 +126,21 @@ function AppHero() {
           </div>
       </div>
       <div className="bestseller">
-        <div>
+        <div className="bestseller-write">
         <h3>Our Best Seller</h3>
         <p>Transform your beauty routine into a masterpiece with our latest cosmetic sensation! 🌟 Unveil the magic of radiant hues, flawless finishes, and a touch of glamour. Elevate your look with a dash of luxury and a sprinkle of confidence. Unleash your inner glow and let your beauty shine, because you're not just using cosmetics – you're creating a masterpiece. Indulge in the extraordinary, embrace your allure!"</p>
+        <div className="subscription-form">
+          <h3>Let’s Connect</h3>
+          <p>Hi! Sign up for our newsletter and be the first to know about exclusive offers, heads up on new product updates and launches, pop-ups events of BeeBeauty Essentials, juicy discounts and more. </p>
+          <form ref={form} onSubmit={sendEmail} className="subscription-main-form">
+              <input type="text" placeholder="Full Name" name="name" required />
+              <input type="email" placeholder="Email Address" name="email" required />
+              <button type="submit" className="submit-btn">Subscribe</button>
+          </form>
+        </div>
         </div>
         <div className="best-seller-pro">
-          <img src={bestseller} width={300}/>
-        </div>
-        <div className="subsciption">
-          
+          <img src={bestseller} />
         </div>
       </div>
       <div className="swipercontainer">
