bc625ea9af7e4f7a83fa101812430f268e4f3268
I_am_Emmanuel <emmaenwereji@gmail.com>
2024-01-16 19:09:40 +0100

    add all files

--- src/routes/Blog.js
+++ src/routes/Blog.js
@@ -5,7 +5,7 @@ import comingsoonimage from "../assets/page_coming_soon.jpg"
 function Blog() {
   return (
     <div className='blog'>
-        <img src={comingsoonimage} />
+        <img src={comingsoonimage} alt='blog image'/>
     </div>
   )
 }
