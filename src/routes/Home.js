3d93b71208fc31dbc6cc94213acafd7415b7940f
I_am_Emmanuel <emmaenwereji@gmail.com>
2024-01-22 14:57:28 +0100

    add all files

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -24,27 +24,27 @@ import { FaQuoteLeft, FaQuoteRight} from "react-icons/fa6";
   const productImageData = [
   {
     id: 1,
-    image: require('../assets/1.jpg'),
+    image: require('../assets/whippe_ butter_ with_tumeric.jpg'),
   },
   {
     id: 2,
-    image: require('../assets/2.jpg'),
+    image: require('../assets/African_black_soap_lavender_honey.jpg'),
   },
   {
     id: 3,
-    image: require('../assets/3.jpg'),
+    image: require('../assets/Sheapleasure_whipped_body_and_hair_butter.jpg'),
   },
   {
     id: 4,
-    image: require('../assets/4.jpg'),
+    image: require('../assets/Aromatherapeutic_liquid_African_black_soap.jpg'),
   },
   {
     id: 5,
-    image: require('../assets/5.jpg'),
+    image: require('../assets/Brightening_liquid_African_Black_soap.jpg'),
   },
   {
     id: 6,
-    image: require('../assets/6.jpg')
+    image: require('../assets/Afican_black_soap_lemongrass_honey.jpg')
   }
 ]
 
