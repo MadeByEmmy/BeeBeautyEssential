bbde19dd627317a96c602631cb7303b2257a9736
I_am_Emmanuel <emmaenwereji@gmail.com>
2024-01-25 16:46:00 +0100

    add file

--- src/routes/Beebeauties.js
+++ src/routes/Beebeauties.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Row, Col, ThemeProvider} from "react-bootstrap"
+import {Row, Col,} from "react-bootstrap"
 import "../components/Beebeauties.css"
 
 //importing from the dataStore
