5a032e9b1cb964c2b0e700086481376440a8a524
I_am_Emmanuel <emmaenwereji@gmail.com>
2024-01-25 12:06:18 +0100

    add file

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -15,7 +15,20 @@ function ProductCard(props) { // props.product is the product we are selling
             <img src={product.image} alt='' style={{ width: '100%', maxWidth: '300px' }}/>
             <Card.Title>{product.title}</Card.Title>
             <Card.Text>₦ {product.price}</Card.Text>
-            <Button variant='primary' onClick={() => cart.addOneToCart(product.id)}>Add To Cart</Button>
+            { productQuantity > 0 ?
+              <>
+                  <Form as={Row}>
+                      <Form.Label column="true" sm="6">In Cart: {productQuantity}</Form.Label>
+                      <Col sm="6">
+                        <Button sm="6" onClick={() => cart.addOneToCart(product.id)} className="mx-2">+</Button>
+                        <Button sm="6" onClick={() => cart.removeOneFromCart(product.id)} className="mx-2">-</Button>
+                      </Col>
+                  </Form>
+                  <Button variant='danger' onClick={() => cart.deleteFromCart(product.id)} className='my-2'>Remove from cart</Button>
+              </>
+              :
+              <Button onClick={() => cart.addOneToCart(product.id)}>Add To Cart</Button>
+            }
         </Card.Body>
     </Card>
   )
