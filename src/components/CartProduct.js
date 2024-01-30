a1646cb484653cb73f462337a65c6ff1c0e6284b
I_am_Emmanuel <emmaenwereji@gmail.com>
2024-01-30 21:27:58 +0100

    add files

--- src/components/CartProduct.js
+++ src/components/CartProduct.js
@@ -9,13 +9,15 @@ function CartProduct(props) {
     const id = props.id;
     const quantity = props.quantity;
     
+    
     const productData = getProductData(id);
 
     return (
         <>
             <img src={productData.image} alt="productimage" style={{ width: '100%'}} />
             <h3>{productData.title}</h3>
-            <p>{quantity} items</p>
+            <p>{quantity} quantities * ₦{productData.price} </p>
+            
             <p>₦{ (quantity * productData.price.toFixed(2)) } </p>
             <Col sm="6">
                         <Button  size="sm" onClick={() => cart.addOneToCart(id)} className="mx-2">+</Button>
