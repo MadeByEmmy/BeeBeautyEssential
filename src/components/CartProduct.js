5eccf46a948ca6ce7f4b22edece2322f87254000
I_am_Emmanuel <emmaenwereji@gmail.com>
2024-02-03 13:13:29 +0100

    add file

--- src/components/CartProduct.js
+++ src/components/CartProduct.js
@@ -4,6 +4,7 @@ import { useContext } from "react";
 import { getProductData } from "../dataStore/beebeautyProductstore";
 
 
+
 function CartProduct(props) {
     const cart = useContext(CartContext);
     const id = props.id;
@@ -13,22 +14,19 @@ function CartProduct(props) {
     const productData = getProductData(id);
 
     return (
-        <div className="cartproduct" style={{width: '100%'}}>
-            <img src={productData.image} alt="productimage" style={{ width: '50%'}} />
-            <div className="product-tittle"><h3>{productData.title}</h3></div>
-            <div className="subtotal"><p>{quantity} quantities * ₦{productData.price} </p></div>
+        <>
+            <img src={productData.image} alt="productimage" />
+            <h3>{productData.title}</h3>
+            <p>{quantity} quantities * ₦{productData.price} </p>
             
-            <div className="total"><p>₦{ (quantity * productData.price.toFixed(2)) } </p></div>
-            <div className="controls">
+            <p>₦{ (quantity * productData.price.toFixed(2)) } </p>
                 <Col sm="6">
-                            <Button  size="sm" onClick={() => cart.addOneToCart(id)} className="mx-2">+</    Button>
-                            <Button  size="sm" onClick={() => cart.removeOneFromCart(id)} className="mx-2">-</Button>
-                            <Button size="sm" onClick={() => cart.deleteFromCart(id)}>Remove</Button>
+                        <Button  size="sm" onClick={() => cart.addOneToCart(id)} className="mx-2">+</    Button>
+                        <Button  size="sm" onClick={() => cart.removeOneFromCart(id)} className="mx-2">-</Button>
                 </Col>
-            </div>
-            
+                <Button size="sm" onClick={() => cart.deleteFromCart(id)}>Remove</Button>
             <hr></hr>
-        </div>
+        </>
     )
 }
 
